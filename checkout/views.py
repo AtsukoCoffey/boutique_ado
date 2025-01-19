5028c205e1bb53155e7b473f7b4ebc938cc8097f
Atsuko Coffey <atsucoffey@gmail.com>
2025-01-19 17:05:41 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Qixz1G4gyOD0nHsxwuxhaXLl6EvEzAr17YCU4sB5UA3wvWfhbHVhxFzsm17AaopTPOTCzGqbfMei0ArymFa21BM00nCwgtLMy',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
